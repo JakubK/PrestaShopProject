33349ea0e1b4f7c33ca6f5651b294ae8e5298583
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-23 13:57:59 +0200

    Create base Product class to model data to scrap

---
+++ Scrapper/Product.cs
@@ -0,0 +1,12 @@
+namespace Scrapper
+{
+    public class Product
+    {
+        public decimal Price {get;set;}
+        public string Name {get;set;}
+        public string Author {get;set;}
+        public string Description {get;set;}
+        public string ImageUrl {get;set;}
+        public string Url {get;set;}
+    }
+}
